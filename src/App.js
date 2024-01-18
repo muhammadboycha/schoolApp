95829506d650dd541d5c46b4d9c3707303b43cf8
muhammadboycha <muhammadboycha@gmail.com>
2024-01-18 23:50:08 +0530

    Signin component cteated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { LoginPage } from './components/Login';
+import { SignIn } from './components/SignIn';
 
 function App() {
   return (
+    <>
+    <SignIn/>
     <LoginPage/>
+    </>
   );
 }
 
 export default App;
+
